6c0e40edc686b7c12375b8f1f8bdd426e8f342f8
Lukeee <trileereize@gmail.com>
2022-10-29 22:30:19 +0800

    Li Leizhi: W6D6

--- src/Compiler.java
+++ src/Compiler.java
@@ -6,8 +6,6 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import src.antlr.*;
@@ -16,9 +14,7 @@ import src.utility.Exception.CompileException;
 
 
 public class Compiler {
-
     public static void main(String[] args) throws IOException {
-        try {
             CharStream charStream = CharStreams.fromStream(System.in);
             MxLexer lexer = new MxLexer(charStream);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -28,9 +24,5 @@ public class Compiler {
             ASTbuilder builder = new ASTbuilder();
             walker.walk(builder, tree);
             builder.check();
-            System.out.println("Success!");
-        }catch (CompileException exception){
-            exception.Call();
-        }
     }
 }
